473ce53c7a4d12f4b589cb11c5ee4959e208b9bb
Tyson Tate <tyson@tysontate.com>
2016-12-13 23:54:01 -0800

    Initial pass at spec subpackage.

---
+++ spec/spec.go
@@ -0,0 +1,143 @@
+package spec
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type Datatype int
+
+const (
+	TextType Datatype = iota
+	BoolType Datatype = iota
+	IntType  Datatype = iota
+)
+
+type Spec struct {
+	Name    string
+	Columns []Column
+}
+
+type Column struct {
+	Name     string
+	Width    int
+	Datatype Datatype
+}
+
+func LoadAll(dir string) (specs []Spec, err error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return specs, err
+	}
+
+	specs = []Spec{}
+	for _, file := range files {
+		path := filepath.Join(dir, file.Name())
+		if file.IsDir() || !strings.HasSuffix(path, ".csv") {
+			log.Printf("Skipping %s", path)
+			continue
+		}
+		spec, err := load(path)
+		if err != nil {
+			log.Printf("WARNING: Couldn't read %s: %s", path, err)
+		}
+
+		specs = append(specs, spec)
+	}
+
+	return specs, nil
+}
+
+func load(path string) (spec Spec, err error) {
+	spec = Spec{
+		Name: stripExtension(filepath.Base(path)),
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return spec, err
+	}
+	defer file.Close()
+
+	r := csv.NewReader(file)
+
+	var headers []string
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return spec, err
+		}
+
+		if headers == nil {
+			headers = record
+		} else {
+			column, err := loadColumn(headers, record)
+			if err != nil {
+				return spec, err
+			}
+			spec.Columns = append(spec.Columns, column)
+		}
+	}
+
+	return spec, nil
+}
+
+func loadColumn(headers, record []string) (col Column, err error) {
+	col = Column{}
+
+	m, err := mapify(headers, record)
+	if err != nil {
+		return col, err
+	}
+
+	col.Name = m["column name"]
+
+	col.Width, err = strconv.Atoi(m["width"])
+	if err != nil {
+		return col, err
+	}
+
+	switch m["datatype"] {
+	case "TEXT":
+		col.Datatype = TextType
+	case "BOOLEAN":
+		col.Datatype = BoolType
+	case "INTEGER":
+		col.Datatype = IntType
+	default:
+		return col, fmt.Errorf("unrecognized datatype: %s", m["datatype"])
+	}
+
+	return col, nil
+}
+
+func mapify(headers, values []string) (m map[string]string, err error) {
+	if len(headers) != len(values) {
+		return nil, fmt.Errorf("row is too short: %#v", values)
+	}
+
+	m = map[string]string{}
+	for i, header := range headers {
+		m[header] = values[i]
+	}
+
+	return m, nil
+}
+
+func stripExtension(name string) string {
+	parts := strings.Split(name, ".")
+	if len(parts) == 1 {
+		return name
+	}
+	return strings.Join(parts[0:len(parts)-1], ".")
+}
---
+++ spec/spec_test.go
@@ -0,0 +1,79 @@
+package spec
+
+import "testing"
+
+func TestLoadAll(t *testing.T) {
+	specs, err := LoadAll("./fixtures")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(specs) != 1 {
+		t.Fatalf("expected 1 spec, got %d", len(specs))
+	}
+}
+
+func TestLoad(t *testing.T) {
+	spec, err := load("./fixtures/testformat1.csv")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.Name != "testformat1" {
+		t.Errorf("expected: %#v, got: %#v", "testformat1", spec.Name)
+	}
+
+	cols := []struct {
+		expectName  string
+		expectWidth int
+		expectType  Datatype
+	}{
+		{"name", 10, TextType},
+		{"valid", 1, BoolType},
+		{"count", 3, IntType},
+	}
+
+	if len(cols) != len(spec.Columns) {
+		t.Fatalf("expected %d columns, got %d", len(cols), len(spec.Columns))
+	}
+
+	for i, col := range cols {
+		actualCol := spec.Columns[i]
+		if col.expectName != actualCol.Name {
+			t.Errorf("cols[%d]: expected: %#v, got: %#v", i, col.expectName, actualCol.Name)
+		}
+		if col.expectWidth != actualCol.Width {
+			t.Errorf("cols[%d]: expected: %#v, got: %#v", i, col.expectWidth, actualCol.Width)
+		}
+		if col.expectType != actualCol.Datatype {
+			t.Errorf("cols[%d]: expected: %#v, got: %#v", i, col.expectType, actualCol.Datatype)
+		}
+	}
+}
+
+func TestLoadColumn(t *testing.T) {
+	// TODO
+}
+
+func TestMapify(t *testing.T) {
+	// TODO
+}
+
+func TestStripExtension(t *testing.T) {
+	tests := []struct {
+		given  string
+		expect string
+	}{
+		{"", ""},
+		{"foo", "foo"},
+		{"bar.txt", "bar"},
+		{"baz.biz.csv", "baz.biz"},
+	}
+
+	for i, test := range tests {
+		got := stripExtension(test.given)
+		if test.expect != got {
+			t.Errorf("tests[%d]: expected %#v, got: %#v", i, test.expect, got)
+		}
+	}
+}
